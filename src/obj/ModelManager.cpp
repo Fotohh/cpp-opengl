052fd74dd56830e1872c59108212f974b2d76580
Xaxis <66885524+Fotohh@users.noreply.github.com>
2026-09-10 23:14:15 -0700

    Refactor remove_model for better performance
    Refactor remove_model to avoid modifying the vector during iteration and improve performance.

--- src/obj/ModelManager.cpp
+++ src/obj/ModelManager.cpp
@@ -4,6 +4,8 @@
 
 #include "ModelManager.h"
 
+#include <algorithm>
+
 void ModelManager::add_model(const Model model) {
     m_models_.push_back(model);
 }
@@ -16,6 +18,16 @@ size_t ModelManager::size() const {
     return m_models_.size();
 }
 
-void ModelManager::remove_model(const Model model) const {
+void ModelManager::remove_model(const Model model) {
     auto it = std::find(m_models_.begin(), m_models_.end(), model);
+    if(it == m_models_.end()) return;
+
+    std::vector<Model> models;
+    models.reserve(m_models_.size() - 1);
+
+    for(auto model_it = m_models_.begin(); model_it != m_models_.end(); model_it++) {
+        if(model_it != it) models.push_back(*model_it);
+    }
+
+    m_models_.swap(models);
 }
